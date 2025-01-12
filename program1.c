a3ae06c7e53ab9ecbc71c77f01d75f2dffe4620f
Manav-020 <u24ai020@aid.svnit.ac.in>
2025-01-12 22:38:17 +0530

    Update program1.c

--- program1.c
+++ program1.c
@@ -1,30 +1,22 @@
 #include<stdio.h>
-
 int main()
 {
     int n;
     printf("Enter size of Bitionic array: ");
     scanf("%d",&n);
-
     int arr[n];
     printf("\nEnter Elements of Bitionic Array\n ");
-    for(int i=0 ; i<n ; i++)
-    {
+    for(int i=0 ; i<n ; i++){
         printf("Enter element %d : ",i+1);
         scanf("%d",&arr[i]);
     }
-
-    for(int i=0 ; i<n ; i++)
-    {
+    for(int i=0 ; i<n ; i++){
         printf("%d ",arr[i]);
     }
-
     int low = 0;
     int high = n-1;
     int mid,max;
-
-    for(int i=0;i<n;i++)
-    {
+    for(int i=0;i<n;i++){
         mid = (high + low)/2;
         if(arr[mid-1]>arr[mid])
         {
@@ -39,6 +31,5 @@ int main()
             max = arr[mid];
         }
     }
-
     printf("\nMaximum Element in Bitionic Array is : %d", max);       
 }
