7ab124a55987170a93d4c8396ed8e22bfc3e8038
Manav-020 <u24ai020@aid.svnit.ac.in>
2025-02-03 17:04:55 +0530

    Create program2.c

---
+++ dsa/assigement5/program2.c
@@ -0,0 +1,68 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+#define n 5
+class stack{
+    public:
+        int top=-1;
+        int arr[n];
+};
+class stack s1;
+void push(int x)
+{
+    // if(top==-1)
+    // {
+    //     s1.top++;
+    //     s1.arr[s1.top]=x;
+    // }
+    if(s1.top==n-1)
+    {
+        cout<<"stack overflow";
+    }
+    else
+    {
+        s1.top++;
+        s1.arr[s1.top]=x;
+    }
+}
+void pop()
+{
+    if(s1.top==-1)
+    {
+        cout<<"stack is empty";
+    }
+    else
+    {
+        cout<<"element"<<s1.arr[s1.top]<<"removed from stack";
+        s1.top--;
+    }
+}
+void peek()
+{
+    cout<<"element at top is"<<s1.arr[s1.top];
+}
+void isFull()
+{
+    if(s1.top==n-1)
+    {
+        cout<<"stack is full";
+    }
+}
+void isEmpty()
+{
+    if(s1.top==-1)
+    {
+        cout<<"stack is empty";
+    }
+}
+int main()
+{
+    int x;
+    cout<<"enter element of stack\n";
+    for (int i=0;i<n;i++)
+    {
+        cout<<"enter element:";
+        cin>> x;
+        push(x);
+    }
+}
